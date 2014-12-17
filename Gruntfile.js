7e5d321a068eff1189e9108b6943c09204a6e99a
dcaruso19 <dcaruso19@gmail.com>
2014-12-17 15:05:59 -0500

    bebry fixed the foundation_custom file and made sass work! menu button now works for medium.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,18 @@
+module.exports = function(grunt) {
 
+   grunt.initConfig({
+       sass: {
+           dist: {
+               files: {
+                   'assets/components/foundation/css/foundation.css': 'assets/components/foundation/scss/foundation_custom.scss',
+                   'assets/components/foundation/css/normalize.css': 'assets/components/foundation/scss/normalize.scss'
+               }
+           }
+       }
+   });
+
+   grunt.loadNpmTasks('grunt-contrib-sass');
+
+   grunt.registerTask('default', ['sass']);
+
+};
